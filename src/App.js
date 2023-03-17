6dd4cd6e0d6d4708743dd8c5ba5fb3f61636e884
yavkanade <irekminnegaliev@gmail.com>
2023-03-17 15:09:09 -0600

    Irek changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Irek is the goat. Throat goat.
         </a>
       </header>
     </div>
