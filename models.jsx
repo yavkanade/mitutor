51b657d7da62c52aed910d878a6359e9c6b495e1
TRBLshoot <Dylanovandermeer@gmail.com>
2023-06-27 18:29:19 -0600

    Added Model.jsx

---
+++ models.jsx
@@ -0,0 +1,14 @@
+User: {
+  name: { type: String}
+  description: {type: String}
+  username: {type: String}
+  password: {type: String}
+  email: {type: String}
+  courses: {type: Array}
+  }
+
+Courses: {
+  name: {type: String}
+  faculty: {type: String}
+  professor: {typr: Array}
+}
--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -4,6 +4,7 @@ import fall from "../assets/fall.mp4";
 
 import { useState, useEffect } from "react";
 import firebase from "../firebase";
+import Courses from "./Courses";
 
 export default function UserHomePage() {
   return (
@@ -13,4 +14,20 @@ export default function UserHomePage() {
       </div>
     </div>
   );
+}
+
+// User: {
+//   name: { type: String}
+//   description: {type: String}
+//   username: {type: String}
+//   password: {type: String}
+//   email: {type: String}
+//   courses: {type: Array}
+//   }
+
+// Courses: {
+//   name: {type: String}
+//   faculty: {type: String}
+//   professor: {typr: Array}
+// }
 }
