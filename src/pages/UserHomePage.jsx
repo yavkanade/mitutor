d186964fc24f39332dfba1c92951a7376092a182
TRBLshoot <Dylanovandermeer@gmail.com>
2023-06-27 18:48:09 -0600

    aDJUSTED PREVIOUS EDIT rEMOVED COMMENTS

--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -15,19 +15,3 @@ export default function UserHomePage() {
     </div>
   );
 }
-
-// User: {
-//   name: { type: String}
-//   description: {type: String}
-//   username: {type: String}
-//   password: {type: String}
-//   email: {type: String}
-//   courses: {type: Array}
-//   }
-
-// Courses: {
-//   name: {type: String}
-//   faculty: {type: String}
-//   professor: {typr: Array}
-// }
-}
